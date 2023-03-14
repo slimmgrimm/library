838cfebd70f05a59e4c946fb221c0fd0d2a6db3f
Slimmgrimmith <dannywukasch@gmail.com>
2023-03-13 21:26:28 -0500

    Initialize html/css/js files

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pageCount, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary() {
+
+}
+
