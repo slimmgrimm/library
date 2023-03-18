48aaa0a69b1454f8ce4463cdb687f45d8e669e91
Slimmgrimmith <dannywukasch@gmail.com>
2023-03-17 21:20:35 -0500

    Created Add Book button

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function Book(title, author, pageCount, hasRead) {
 }
 
 function addBookToLibrary() {
+    newTitle = prompt('What is the title of the book?');
+    newBook = new Book(newTitle, 'Dan', '187', true);
+    console.log(newBook);
 
+    // bookCard = document.createElement('p');
+    // bookCard.textContent = newBook.title;
+    // bookCard.appendChild(newTitle);
 }
+
+
+
 
