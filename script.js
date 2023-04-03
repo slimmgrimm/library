8db23b411f09d9fec693dcfe2c8b8a8d321865ae
Slimmgrimmith <dannywukasch@gmail.com>
2023-04-03 11:23:19 -0500

    Added DOM book creation

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-let myLibrary = [];
+// let myLibrary = [];
 
-function Book(title, author, pageCount, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.hasRead = hasRead;
+// function Book(title, author, pageCount, hasRead) {
+//     this.title = title;
+//     this.author = author;
+//     this.pageCount = pageCount;
+//     this.hasRead = hasRead;
+// }
+
+function addBookToLibrary(book) {
+    title = prompt('What is the title?');
+    const header = document.querySelector('header');
+    const myH1 = document.createElement('h1');
+    myH1.textContent = title;
+    header.appendChild(myH1);
 }
 
-function addBookToLibrary() {
-    newTitle = prompt('What is the title of the book?');
-    newBook = new Book(newTitle, 'Dan', '187', true);
-    console.log(newBook);
+    // newTitle = prompt('What is the title of the book?');
+    // newBook = new Book(newTitle, 'Dan', '187', true);
+    // console.log(newBook);
 
     // bookCard = document.createElement('p');
     // bookCard.textContent = newBook.title;
     // bookCard.appendChild(newTitle);
-}
+// }
 
 
 
